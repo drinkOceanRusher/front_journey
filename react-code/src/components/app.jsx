2f2344a85327fcebdc5191bca6036a471a757a71
zhangfei <940543588@qq.com>
2020-12-06 13:45:51 +0800

    初始化blank项目

---
+++ react-code/src/components/app.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import log from '../logo.svg'
+class App extends Component{
+    render() {
+        return(
+            <div>
+                <img src={log} alt="log"/>
+            </div>
+        )
+    }
+}
+
+export default App
