b02d963f969873fba1e059cb1789e7a0768518d9
zhangfei <940543588@qq.com>
2020-12-06 19:20:27 +0800

    入口js及第一个组件

--- react-code/src/components/app.jsx
+++ react-code/src/components/app.jsx
@@ -1,4 +1,11 @@
 import React, {Component} from 'react'
+
+/**
+ * 定义组件2种方式 简单：函数；复杂：类
+ * Component是React的属性，所以 import React, {Component} from 'react' ——》直接用Componet
+ * import Reactfrom 'react'  extends React.Component
+ */
+
 import log from '../logo.svg'
 class App extends Component{
     render() {
--- react-code/src/index.js
+++ react-code/src/index.js
@@ -2,4 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from "./components/app";
 
+/**
+ * 包名小写
+ * 自定义模块一定是.开头的相对路径
+ */
 ReactDOM.render(<App/> ,document.getElementById('root'))
