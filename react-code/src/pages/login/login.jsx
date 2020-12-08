ab0d566cb3e264906d62816f510bbfcc04961cfe
zhangfei <940543588@qq.com>
2020-12-08 21:48:06 +0800

    使用antd的表单组件完成登录

--- react-code/src/pages/login/login.jsx
+++ react-code/src/pages/login/login.jsx
@@ -1,9 +1,70 @@
-import React,{Component} from "react";
+import React, {Component} from "react";
+import './login.less'
+import logo from './images/logo.png'
+import {Form, Icon, Input, Button} from 'antd';
 
-export default class Login extends Component{
+
+ class Login extends Component {
+     handleSubmit = e => {
+         e.preventDefault();
+         this.props.form.validateFields((err, values) => {
+             if (!err) {
+                 console.log('Received values of form: ', values);
+             }
+         });
+     };
+
     render() {
-        return(
-            <div>login页</div>
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <div className='login'>
+
+                <header className='login-header'>
+                    <img src={logo} alt='logo'/>
+                    <h1>React项目 后台管理系统</h1>
+                </header>
+                <section className='login-content'>
+                    <Form className="login-form" onSubmit={this.handleSubmit}>
+                        <Form.Item>
+                            {getFieldDecorator('username', {
+                                rules: [
+                                    { required: true, message: '请输入用户名' },
+                                    { min: 4, message: '用户名至少4位' },
+                                    { max: 12, message: '用户名最多12位' }
+                                ],
+                            })(
+                                <Input
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    placeholder="Username"
+                                />,
+                            )}
+
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: '请输入密码' }],
+                            })(
+                                <Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    placeholder="Password"
+                                />,
+                            )}
+
+                        </Form.Item>
+                        <Form.Item>
+
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Log in
+                            </Button>
+
+                        </Form.Item>
+                    </Form>
+                </section>
+            </div>
         )
     }
 }
+
+const WrapLogin = Form.create()(Login)
+export default WrapLogin
